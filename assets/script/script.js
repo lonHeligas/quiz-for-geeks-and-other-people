563ab74f8a1a85fb20f887dd8e44d091e5dd7723
Lon Heligas <lon.heligas@gmail.com>
2023-01-04 17:59:01 -0600

    more setup

---
+++ assets/script/script.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// click the start button
+// timer starts
+// question shows
+// click the answer
+// record result
+//  if incorrectd answer, subtract 5 seconds from the clock
+// present new question
+
+// timer reaches 0
+//  game is over
+// prompt user to save initials and score
+// which will persist in reloading the page
