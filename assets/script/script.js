6d3ae15a3b49e91134b090b7fa3432a47550f386
Lon Heligas <lon.heligas@gmail.com>
2023-01-04 20:58:08 -0600

    added timer function and tracked it

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,13 @@
-var quizTitle = document.querySelector("#title");
-var quizInst = document.querySelector("#instructions");
-var quizAnswers = document.querySelector("#options");
-var quizQuestion = document.querySelector("#questions");
-var quizTimer = document.querySelector("#timer");
-var highScores = document.querySelector('highscores')
-var startButton = document.querySelector("#start");
+const quizTitle = document.querySelector("#title");
+const quizInst = document.querySelector("#instructions");
+const quizAnswers = document.querySelector("#options");
+const quizQuestion = document.querySelector("#questions");
+const quizTimer = document.querySelector("#timer");
+const highScores = document.querySelector('highscores')
+const startButton = document.querySelector("#start");
 
+let currentScore = 0;
+var timer = 100;
 
 var allQuestions = [
   { questionNumber: "1", answer1: "What", answer2: "Where", answer3: "Why", correctAnswer: 2},
@@ -13,10 +15,8 @@ var allQuestions = [
   { questionNumber: "3", answer1: "What", answer2: "Where", answer3: "Why", correctAnswer: 1}
 ]
 
-
 function introPage(){
-  // console.log(allQuestions)
-
+  console.log(allQuestions)  
   // primary intro page
   quizTitle.textContent = 'Coding Quiz Challenge';
   // console.log("hello world");
@@ -29,8 +29,26 @@ function introPage(){
   })
   // quizRunner ();
 }
+// where am I in the quiz? pre/in/results
+// in the quiz, what's gong on? the timer and the score
+// things that are happening in the one question
+// 
 
-function quizRunner(){
+function quizRunner(){  
+  // set the timer to 100 seconds
+  // what happens when it ticks   
+
+  setInterval(function(){
+    console.log(timer);
+    timer--;
+    if (timer === 0){
+      // go to the show your score and put your name in screen
+      
+    } else {
+      // keep going
+    }
+  }, 1000);
+
   // startTheTimer();
 
   // blank out the prior text fields and start button
@@ -61,16 +79,20 @@ introPage()
 
 
 
-// click the start button
+// click the start button - DONE
 // timer starts
 // question shows
+//    I've populated the question fields correctly
+//    
 // click the answer
 // record result
-//  if incorrectd answer, subtract 5 seconds from the clock
+//  if incorrectd answer, subtract 10 seconds from the clock
 // present new question
 
 // timer reaches 0
 //  game is over
 // prompt user to save initials and score
 // which will persist in reloading the page
+
+
 // startQuiz ();
