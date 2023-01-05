cb8ddcc0d9a35ab8bbb9213f917a0e0c16ac7f5a
Lon Heligas <lon.heligas@gmail.com>
2023-01-04 22:10:48 -0600

    populated the answers as buttons and display them on page

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,7 @@
 const quizTitle = document.querySelector("#title");
 const quizInst = document.querySelector("#instructions");
 const quizQuestion = document.querySelector("#question");
+const questionNum = document.querySelector("#questionNum");
 const quizAnswers = document.querySelector("#answers");
 const quizTimer = document.querySelector("#timer");
 const highScores = document.querySelector('highscores')
@@ -11,14 +12,15 @@ var timer = 100;
 let currentQuestion = 0;
 
 var allQuestions = [
-  { questionNumber: "1", questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer1: "What1", answer2: "Where1", answer3: "Why1", correctAnswer: 2},
-  { questionNumber: "2", questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer1: "What2", answer2: "Where2", answer3: "Why2", correctAnswer: 3},
-  { questionNumber: "3", questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer1: "What3", answer2: "Where3", answer3: "Why3", correctAnswer: 1}
+  { questionNumber: "1", questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What1", "Where1", "Why1"],correctAnswer: 1},
+  { questionNumber: "2", questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What2", "Where2", "Why2"], correctAnswer: 2},
+  { questionNumber: "3", questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What3", "Where3", "Why3"], answer3: "Why3", correctAnswer: 0}
 ]
 
 
 function introPage(){
   console.log(allQuestions)  
+  console.warn("yo!");
   // primary intro page
   quizTitle.textContent = 'Coding Quiz Challenge';
   // console.log("hello world");
@@ -27,6 +29,7 @@ function introPage(){
   quizInst.textContent = "Try to answer the following questions about geeky things within the time limit. Keep in mind that incorrect answers will penalize your scoretime by ten seconds! Oh noes!"
   startButton.textContent = "Start!"    
   startButton.addEventListener("click", function(){
+    startButton.remove();
     quizRunner();
   })
   // quizRunner ();
@@ -62,10 +65,25 @@ function quizRunner(){
   // allQuestions[2].answer1
   allQuestions[currentQuestion]
 
+
+  
+  questionNum.textContent = `Question Numero ${currentQuestion+1}`;
+  
   quizQuestion.textContent = allQuestions[0].questionText;
-  quizAnswers.append(allQuestions[0].answer1);
-  quizAnswers.append(allQuestions[0].answer2);
-  quizAnswers.append(allQuestions[0].answer3);
+  // displays the question answers from the array
+  for (i=0; i<allQuestions[0].answer.length; i++){
+    var quizButton = document.createElement("button");
+    quizButton.textContent = allQuestions[0].answer[i];
+    quizAnswers.append(quizButton);    
+    // make a button
+
+
+
+
+  }
+
+
+
 
   // Node.operation.value
   
