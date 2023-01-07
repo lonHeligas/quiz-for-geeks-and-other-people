37f8ccfee02a406251f238674d0840327d3e01b4
Lon Heligas <lon.heligas@gmail.com>
2023-01-07 13:00:19 -0600

    reorganization of html and script file complete and sections are working again

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,7 @@
 const introBoard = document.querySelector('#intro-board');
 const quizBoard = document.querySelector('#quiz-board');
 const highScoreBoard = document.querySelector('#high-score-board');
-const gotoHiScore = document.querySelecdtor('go-to-hiscore');
+const gotoHiScore = document.querySelector('go-to-hiscore');
 const quizTitle = document.querySelector('#title');
 const quizInst = document.querySelector('#instructions');
 const quizQuestion = document.querySelector('#question');
@@ -16,26 +16,23 @@ const highScorePrompt = document.querySelector('#high-score-prompt');
 const highScoreField = document.querySelector('#entry-field');
 const submitButton = document.querySelector('#submit-button');
 const highScoreLine = document.querySelector('#high-score-line');
-
-const retakeQuiz = documenty.querySelecdtor('#retakeQuiz');
-
-// when styling everything, put each screen in its own div. then you can just turn off the div itself and not all of the elements.
+const retakeQuiz = document.querySelector('#retakeQuiz');
 
 
-
+// idea: when styling everything, put each screen in its own div. then you can just turn off the div itself and not all of the elements. (this borked everything lol)
 
 let quizButton = document.createElement("button");
 let currentScore = 0;
-let secondsLeft = 20;
 let currentQuestion = 0;
+let secondsLeft = 10;
 
 var allQuestions = [
-  {questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What1", "Where1!!!", "Why1"],correctAnswer: 1},
+  {questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["One", "3", "Two"],correctAnswer: 1},
   {questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What2", "Where2", "Why2!!!"], correctAnswer: 2},
-  {questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What3!!!", "Where3", "Why3"], answer3: "Why3", correctAnswer: 0},
-  {questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What4!!!", "Where4", "Why4"], answer3: "Why3", correctAnswer: 0},
-  {questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What5!!!", "Where5", "Why5"], answer3: "Why3", correctAnswer: 0},
-  {questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What6!!!", "Where6", "Why6"], answer3: "Why3", correctAnswer: 0}
+  {questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What3!!!", "Where3", "Why3"], correctAnswer: 0},
+  {questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What4!!!", "Where4", "Why4"], correctAnswer: 0},
+  {questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What5!!!", "Where5", "Why5"], correctAnswer: 0},
+  {questionText: "How many licks does it take to get to the center of a Tootsie Roll Pop?", answer:["What6!!!", "Where6", "Why6"], correctAnswer: 0}
 ]
 
 let hiScores = localStorage.getItem("playerScoreEntry");
@@ -52,19 +49,15 @@ console.log(hiScores);
 
 
 
-function introPage(){
-  
-  // console.log(allQuestions)  
-  // console.warn("yo!");
-
+function introPage(){   
   // primary intro page
   quizTitle.textContent = 'Geeks and Non-Geeks Quiz';
-  submitButton.classList.add('hidden');
-  // console.log("hello world");
-  // console.log(quizTitle)
+  // submitButton.classList.add('hidden');
+
+    // console.log(quizTitle)
   
   quizTimer.textContent = `Time: ${secondsLeft}`;
-  quizInst.textContent = "Try to answer the following questions about geeky things within the time limit. Keep in mind that incorrect answers will penalize your scoretime by ten seconds! Oh noes!"
+  quizInst.textContent = "Try to answer the following questions about geeky things within the time limit. Keep in mind that incorrect answers will penalize your scoretime by ten seconds!"
   startButton.textContent = "Start!"    
   startButton.addEventListener("click", function(){
     startButton.remove();    
@@ -97,14 +90,16 @@ function startTimer(){
     
   }, 1000);
   quizRunner();
-  
+  // also put in if you're out of questions in line 79
 }
 
 function quizRunner(){
-  // console.log("You are in quizRunner");
-  // clears out title and instructions  
+  quizBoard.classList.remove('hidden');
+  introBoard.classList.add('hidden');
   
-  clearPage();
+  // console.log("You are in quizRunner");
+  
+
   
   // populate the quiz question AND get the answers as buttons  
   allQuestions[currentQuestion]  
@@ -113,6 +108,7 @@ function quizRunner(){
   
   // console.log(quizQuestion)
   
+  quizAnswers.innerHTML = '';
   // displays the question answers from the array
   for (i=0; i<allQuestions[currentQuestion].answer.length; i++){
     let myBtn = document.createElement('button');
@@ -122,9 +118,7 @@ function quizRunner(){
     }
     // console.warn(myBtn);
     myBtn.addEventListener("click", checkAnswers)
-    quizAnswers.appendChild(myBtn);    
-    
-    
+    quizAnswers.appendChild(myBtn);  
   }  
   // console.log(window);
 }
@@ -132,13 +126,14 @@ function quizRunner(){
 function checkAnswers(event){ 
   // checks the answer to see if it's right or wrong and adjusts score or time accodingly
   event.preventDefault()
+  console.warn(quizFeedback);
   // console.log(event.target)
   // console.log(event)
   if(event.target.hasAttribute("correct")){
     // console.log("Great!")
+    // updates the score
     currentScore++;
     quizFeedback.textContent = "Correct!";
-    // update the score
   } else{
     // console.log("WRONG")    
     secondsLeft-=10;  
@@ -148,16 +143,21 @@ function checkAnswers(event){
   quizRunner();
 }
 
-function gameOver(){    
-  clearPage()
+function gameOver(){ 
+  const quizIsOver = document.querySelector('#quiz-is-over');
+  const finalScoreIs = document.querySelector('#final-score');
+  
+  console.log("You are in gameOver()");
+  quizBoard.classList.add('hidden');
+  highScoreEntry.classList.remove('hidden');
+  // clearPage()
   // adds the submit button back onto the page
   submitButton.classList.remove('hidden');
   submitButton.enable;
-  submitButton.textContent = "Submit";  
+  // submitButton.textContent = "Submit";  
   console.log(submitButton);
-  console.log("You are in gameOver()");
-  quizTitle.textContent = "The Quiz is over!";
-  quizInst.textContent = `Your final score is: ${currentScore}`; 
+  quizIsOver.textContent = "The Quiz is over!";
+  finalScoreIs.textContent = `Your final score is: ${currentScore}`; 
   highScorePrompt.textContent = "Enter your initials and click Submit";
   highScoreField.classList.remove('hidden'); 
   // listens for the user to click the submit button to store in an object
@@ -174,18 +174,30 @@ function gameOver(){
   })    
 }
 
-function clearPage(){
-  // console.log("CLEAR THE PAAAGGEEEE");  
-  quizInst.textContent = ``;
-  quizQuestion.textContent = ``;
-  quizAnswers.textContent = ``;
-  questionNum.textcontent = ``;
-  quizFeedback.textContent = ``;  
-}
-
-function displayHighScores(){
-  // displays the high score list
-  clearPage();
+// function clearPage(){
+  
+  
+  
+  //   // console.log("CLEAR THE PAAAGGEEEE");  
+  //   // introBoard.textContent = ``;
+  //   // quizBoard.textContent = ``;
+  //   // highScoreBoard.textContent = ``;
+  
+  
+  //   // quizInst.textContent = ``;
+  //   // quizQuestion.textContent = ``;
+  //   // quizAnswers.textContent = ``;
+  //   // questionNum.textcontent = ``;
+  //   // quizFeedback.textContent = ``;  
+  // }
+  
+  function displayHighScores(){
+    console.log("you are in display high scores");
+    highScoreEntry.classList.add('hidden');
+    highScoreBoard.classList.remove('hidden');
+    
+    // displays the high score list
+  
   // runs through the high scores stored in local memory and displays them
   for (i=0; i<hiScores.length; i++){  
     let myHiScore = document.createElement('li');
